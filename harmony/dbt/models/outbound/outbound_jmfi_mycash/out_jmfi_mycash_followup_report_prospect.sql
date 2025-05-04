36e34d2b952de51e75fedf8c32ac1417d2b81467
royandhika <satriaroy70@gmail.com>
2025-05-04 21:54:58 +0700

    feat(harmony): add 2 tables on jmfi mycash
    Add column valid_status on out_jmfi_mycash_report_prospect (& _timeframe)
    Add 2 tables out_jmfi_mycash_followup_report_prospect (& _timeframe)

---
+++ harmony/dbt/models/outbound/outbound_jmfi_mycash/out_jmfi_mycash_followup_report_prospect.sql
@@ -0,0 +1,92 @@
+{{
+	config(
+		materialized='incremental',
+		schema=env_var('ENV_SCHEMA') + '_dm',
+        unique_key=['data_supply_id', 'call_type'],
+        group='outbound_jmfi_mycash',
+		tags=['12hourly']
+	)
+}}
+
+with src as (
+	select 
+		*
+		,interest_date = min(case 
+			when campaign_result_detail = 'Interest - Order' then interaction_date
+			else '9999-12-31'
+		end) over(partition by data_supply_id)
+	from {{ ref('out_jmfi_mycash_report_prospect_timeframe') }}
+	{% if is_incremental() %}
+	where uploaddate >= '{{ var('min_date') }}'
+		and uploaddate <= '{{ var('max_date') }}'
+	{% endif %}
+)
+,attempt as (
+	select 
+        *
+		,last_response = row_number() over(partition by data_supply_id, case 
+			when interaction_date <= interest_date then 'regular'
+			when interaction_date > interest_date then 'follow_up'
+		end order by interaction_date asc)
+		,rn = row_number() over(partition by data_supply_id, case 
+			when interaction_date <= interest_date then 'regular'
+			when interaction_date > interest_date then 'follow_up'
+		end order by interaction_date desc)
+		,call_type = case 
+			when interaction_date <= interest_date then 'regular'
+			when interaction_date > interest_date then 'follow_up'
+		end
+	from src
+)
+,final as (
+    select 
+        DataSupplyTenor
+		,agreement_no
+		,angsuran
+		,appointment_date
+		,area_business
+		,asset_code
+		,assignment_date
+		,attempt = last_response
+		,book_criteria_cust
+		,branch_full_name_2
+		,campaign_result
+		,campaign_result_detail
+		,cat_product
+		,connected_result
+		,contacted_result
+		,current_assigned_agent_email
+		,current_assigned_agent_id
+		,current_assigned_agent_name
+		,customer_id
+		,customer_name
+		,data_supply_date
+		,data_supply_id
+		,data_supply_submission_id
+		,disburse
+		,interaction_date
+		,interaction_id
+		,is_appointment
+		,is_hot_prospect
+		,is_interest_waiting_for_order_need_follow_up
+		,is_need_follow_up
+		,jenis
+		,merk
+		,notes
+		,otr
+		,phone_number
+		,profession
+		,reference_phone_number
+		,[source]
+		,status_kepemilikan_rumah
+		,tahun_kendaraan
+		,tenor
+		,tenor_left
+		,tipe
+		,valid_status
+		,call_type
+		,uploaddate = getdate()
+    from attempt 
+    where rn = 1
+)
+select * from final
